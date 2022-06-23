3c0d136666c1a4b6f1806356568a7a3ef34b8b98
mitaligarg027 <94614437+mitaligarg027@users.noreply.github.com>
2022-06-23 12:03:19 +0530

    Create mitalisrepository.cpp

---
+++ mitalisrepository.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		int space=2*n-2*i;
+		for(j=1;j<=space;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	for(i=n;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		int space=2*n-2*i;
+		for(j=1;j<=space;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
